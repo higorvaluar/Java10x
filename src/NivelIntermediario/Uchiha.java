d8f5e5875a172d4e06e6841d244ea2ecc7da81ee
Higor Valuar <168334690+higorvaluar@users.noreply.github.com>
2024-11-23 18:15:05 -0300

    Aula de Interface

---
+++ src/NivelIntermediario/Uchiha.java
@@ -0,0 +1,9 @@
+package NivelIntermediario;
+
+public class Uchiha extends Ninja {
+
+    // Sharingan ativado, método público
+    public void SharinganAtivado() {
+        System.out.println("Meu nome é " + nome + ". O Sharingan Ativou, eu sou um Uchiha!");
+    }
+}
---
+++ src/NivelIntermediario/Uzumaki.java
@@ -0,0 +1,7 @@
+package NivelIntermediario;
+
+public class Uzumaki extends Ninja {
+    public void chakraPraCaralho() {
+        System.out.println("Meu nome é " + nome + ". E eu tenho Chakra para um caralho, então sou um Uzumaki!");
+    }
+}
