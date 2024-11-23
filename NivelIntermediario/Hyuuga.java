5244d68da5ebb51fd7933f2e6e3c8a223894be3d
Higor Valuar <168334690+higorvaluar@users.noreply.github.com>
2024-11-23 18:14:27 -0300

    Delete NivelIntermediario directory

--- NivelIntermediario/Hyuuga.java
+++
@@ -1,7 +0,0 @@
-package NivelIntermediario;
-
-public class Hyuuga extends Ninja{
-    public void ativarByakugan() {
-        System.out.println("Me chamo " + nome + ". Ativei o Byakugan, então sou uma Hyuuga!");
-    }
-}
