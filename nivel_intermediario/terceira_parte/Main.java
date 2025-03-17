084fd18cde23458d7c7f52f466d275621ef82c9e
higorvaluar <higorvb2001@gmail.com>
2025-03-17 18:13:25 -0300

    List

--- nivel_intermediario/terceira_parte/Main.java
+++ nivel_intermediario/terceira_parte/Main.java
@@ -1,15 +1,38 @@
 package nivel_intermediario.terceira_parte;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
-        Missoes missao1 = new Missoes("Resgatar um gato", RankDeMissoes.E);
-        missao1.exibirDetalhes();
+        // Arrays são estáticos, não alteram de tamanho
+        String[] ninjasArray = new String[3];
+        ninjasArray[0] = "Naruto Uzumaki";
+        ninjasArray[1] = "Sasuke Uchiha";
+        ninjasArray[2] = "Sakura Haruno";
+        System.out.println("Printando array = " + ninjasArray[0]);
 
-        System.out.println("-------------------------------------");
+        // Listas não são estáticas, elas podem aumentar e diminuir
+        List<String> ninjasList = new ArrayList<>();
 
-        Missoes missao2 = new Missoes("Matar o Madara Uchiha", RankDeMissoes.SS);
-        missao2.exibirDetalhes();
+        // Adicionar na lista
+        ninjasList.add("Sarada Uchiha"); // Index 0
+        ninjasList.add("Tobirama Senju"); // Index 1
+        ninjasList.add("Hashirama Senju"); // Index 2
+        ninjasList.add("Kakashi Hatake"); // Index 3
 
+        System.out.println("Printando List = " + ninjasList);
+
+        // Remover da lista
+        ninjasList.remove("Kakashi Hatake");
+        System.out.println("Printando List = " + ninjasList);
+
+        // Trocar elementos
+        ninjasList.set(1, "Minato Namikaze");
+        System.out.println("Printando List = " + ninjasList);
+
+        // Ver tamanho da lista
+        System.out.println("Tamanho da lista: " + ninjasList.size() + " elementos.");
     }
 }
